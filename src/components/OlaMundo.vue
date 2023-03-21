e60b5098a8a9738acfc1ab85d0adf9b8441b2393
soareslil <lilianectd@gmail.com>
2023-03-21 17:01:17 -0300

    starting vue project

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello World, with VUE.js</h1>
+    <p>My first Vue project</p>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "OlaMundo",
+});
+</script>
+
+<style>
+h1{
+    color: "blue";
+},
+p{
+    color: "pink";
+}
+
+</style>
