d02ed2a032eaf6dae25195cb6059ca4f813682d8
soareslil <lilianectd@gmail.com>
2023-03-24 20:43:29 -0300

    preparando modo escuro

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline modo-escuro">
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
       <FormularioBase @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
         <TarefaBase v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
@@ -53,5 +53,18 @@ export default defineComponent({
 <style>
 .lista {
   padding: 1.24rem;
+}
+main{
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro{
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo{
+  background-color: var(--bg-primario);
 }
 </style>
